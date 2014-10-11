a855794b9b154537b013964a1889699b2c28e919
fabienL <lailliaufabien@gmail.com>
2014-10-12 01:06:29 +0200

    add creator

---
+++ app/scripts/directives/creators.js
@@ -0,0 +1,13 @@
+'use stric';
+
+angular.module('marvel.directives')
+.directive('creators', function(){
+	return {
+      restrict: 'E',
+      scope: {creators: '=info'},
+      templateUrl: 'partials/creators.html',
+      link : function(scope,element, attrs){
+     
+      }    
+    };
+})
